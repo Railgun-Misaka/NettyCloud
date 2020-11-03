e4e094ade441d7db1ec23f4a93b1ed1c0a078d77
fly <362507346@qq.com>
2020-11-03 17:34:20 +0800

    消息总线

--- cloud-common/src/main/java/com/fly/cloud/entity/ZztThroughBusiness.java
+++ cloud-common/src/main/java/com/fly/cloud/entity/ZztThroughBusiness.java
@@ -2,6 +2,11 @@ package com.fly.cloud.entity;
 
 import java.util.Date;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
 public class ZztThroughBusiness {
 	
 	private String business_id ;
@@ -9,29 +14,5 @@ public class ZztThroughBusiness {
 	private Date create_time ;
 	
 	private String business_code ;
-	
-	public String getBusiness_id() {
-		return business_id;
-	}
-
-	public void setBusiness_id(String business_id) {
-		this.business_id = business_id;
-	}
-
-	public Date getCreate_time() {
-		return create_time;
-	}
-
-	public void setCreate_time(Date create_time) {
-		this.create_time = create_time;
-	}
-
-	public String getBusiness_code() {
-		return business_code;
-	}
-
-	public void setBusiness_code(String business_code) {
-		this.business_code = business_code;
-	}
 
 }
--- cloud-config-server/src/main/java/com/fly/cloud/ConfigServerApplication.java
+++ cloud-config-server/src/main/java/com/fly/cloud/ConfigServerApplication.java
@@ -5,14 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.PropertySource;
+
+import com.fly.cloud.factory.YmlPropertySourceFactory;
 
 
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
 @EnableEurekaClient
+@PropertySource(value = "${spring.cloud.config.server.native.searchLocations}native-common.yml", factory = YmlPropertySourceFactory.class)
 public class ConfigServerApplication {
-	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class);
